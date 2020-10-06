7c9b9aa41bacbc158c965aeb45f74e9bf6bc5f69
marco76tv <marco.sottana@gmail.com>
2020-10-06 14:06:30 +0200

    .

---
+++ composer_init.sh
@@ -0,0 +1,15 @@
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+php -d memory_limit=-1 composer.phar require  doctrine/dbal
+php -d memory_limit=-1 composer.phar require  laravel/scout
+php -d memory_limit=-1 composer.phar require  laravel/slack-notification-channel
+php -d memory_limit=-1 composer.phar require  laravel/socialite
+php -d memory_limit=-1 composer.phar require  laravelcollective/html
+php -d memory_limit=-1 composer.phar require  mcamara/laravel-localization
+php -d memory_limit=-1 composer.phar require  nwidart/laravel-modules
+php -d memory_limit=-1 composer.phar require  staudenmeir/eloquent-has-many-deep
+php -d memory_limit=-1 composer.phar require  unisharp/laravel-filemanager
+php -d memory_limit=-1 composer.phar require  laravel/ui
+
+php -d memory_limit=-1 composer.phar require  --dev barryvdh/laravel-debugbar
